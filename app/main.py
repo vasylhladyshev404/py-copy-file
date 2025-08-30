aa9c7329c87dfe93da594bab81e47daa889bccea
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 10:37:12 +0200

    1

--- app/main.py
+++ app/main.py
@@ -9,5 +9,6 @@ def copy_file(command: str) -> None:
     if src == dst:
         return
 
-    with open(src, "r", encoding="utf-8") as file_in, open(dst, "w", encoding="utf-8") as file_out:
-        file_out.write(file_in.read())
+    with open(src, "r", encoding="utf-8") as file_in, \
+         open(dst, "w", encoding="utf-8") as file_out:
+        file_out.write(file_in.read())      
