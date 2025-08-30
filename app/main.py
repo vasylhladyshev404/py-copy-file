6997b9284903993c1d5d37fee42bb3e5e28f17fe
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 10:29:09 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return 
+
+    _, src, dst = parts
+
+    if src == dst:
+        return
+
+    with open(src, "r", encoding="utf-8") as file_in, open(dst, "w", encoding="utf-8") as file_out:
+        file_out.write(file_in.read())
